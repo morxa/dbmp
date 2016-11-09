74ac2d559227659d00eaf330de6cb50d2cd6cb92
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2016-11-09 12:16:16 +0100

    scripts: add script to upload PDDL files to the plan database
    The script takes a domain file and/or a problem file and uploads it to
    the database. It does several sanity checks:
    - When uploading a domain or a problem, the domain/problem must not
      exist in the database.
    - When uploading a problem, the corresponding domain must be in the
      database.
    - When uploading a problem, the domain specified on the command line
      must match the domain given in the problem file.

---
+++ scripts/upload_pddl.py
@@ -0,0 +1,163 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+#  Created:  Tue 08 Nov 2016 07:25:09 PM CET
+#  Copyright  2016  Till Hofmann <hofmann@kbsg.rwth-aachen.de>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Library General Public License for more details.
+#
+#  Read the full text in the LICENSE.GPL file in the doc directory.
+
+"""
+Upload a domain file or a problem file to the database
+"""
+
+import argparse
+import getpass
+import pymongo
+import re
+
+def get_domainname(domain_string):
+    """Gets the domain name from the domain given as string.
+
+    Parses the first line of the string and returns the domain name specified in
+    the domain string.
+
+    Args:
+        domain_string: A string representation of the domain.
+
+    Returns:
+        A domain as string.
+
+    Raises:
+        AssertionError: The domain string does not contain a domain name.
+    """
+    for line in domain_string.splitlines():
+        # Skip empty lines.
+        if re.match(r'^\s*$', line):
+            continue
+        match = re.search('\(domain ([^)]+)\)', line)
+        assert match is not None, \
+            'Could not extract domain name from ' \
+            'first non-empty line "{}".'.format(line)
+        return match.group(1)
+
+def get_problemname(problem_string):
+    """Get the problem name from the problem given as string.
+
+    Parses the first lines of the string until it finds the problem definition
+    and returns the problem name.
+
+    Args:
+        problem_string: A string representation of the problem.
+
+    Returns:
+        The problem name as string.
+
+    Raises:
+        AssertionError: The problem name could not be found.
+    """
+    for line in problem_string.splitlines():
+        # Skip empty lines.
+        if re.match(r'^\s*$', line):
+            continue
+        match = re.search('\(problem ([^)]+)\)', line)
+        assert match is not None, \
+            'Could not extract problem name from ' \
+            'first non-empty line "{}".'.format(line)
+        return match.group(1)
+
+def get_domain_of_problem(problem_string):
+    """Get the domain of the given problem.
+
+    Parses the problem string and returns the domain name of the problem.
+    
+    Args:
+        problem_string: A string representation of the problem.
+
+    Returns:
+        The domain name of the domain this problem is part of.
+
+    Raises:
+        AssertionError: The domain name could not be found.
+    """
+    for line in problem_string.splitlines():
+        # Skip empty lines.
+        if re.match(r'^\s*$', line):
+            continue
+        match = re.search('\(:domain ([^)]+)\)', line)
+        if match:
+            break
+    assert match is not None, \
+        'Could not extract domain name from ' \
+        'first non-empty line "{}".'.format(line)
+    return match.group(1)
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Upload a PDDL domain or problem file to the database')
+    parser.add_argument('-H', '--db-host', default='localhost',
+                        help='the database hostname')
+    parser.add_argument('-u', '--db-user', default='planmgr',
+                        help='the database username')
+    parser.add_argument('-p', '--db-passwd',
+                        help='the database password')
+    parser.add_argument('--domainfile', help='the domain file to add')
+    parser.add_argument('--domain', help='the domain the problems belong to')
+    parser.add_argument('problems', metavar='problemfile', nargs='*',
+                        help='the problem files to add')
+    args = parser.parse_args()
+    if not args.db_passwd:
+        print('Please enter the database password:')
+        args.db_passwd = getpass.getpass()
+    client = pymongo.MongoClient(host=args.db_host)
+    database = client.macro_planning
+    database.authenticate(args.db_user, args.db_passwd)
+    domain_coll = client.macro_planning.domains
+    problem_coll = client.macro_planning.problems
+    if args.domainfile:
+        domainfile = open(args.domainfile, 'r')
+        domain_string = domainfile.read()
+        domain = get_domainname(domain_string)
+        if args.domain:
+            assert domain == args.domain, \
+                'Domain "{}" in domain file does not match ' \
+                'given domain "{}".'.format(domain, args.domain)
+        assert domain_coll.find({ 'name': domain }).count() == 0, \
+            'Domain "{}" already exists in the database.'.format(domain)
+        print('Adding domain "{}" to the database.'.format(domain))
+        domain_coll.insert({ 'name': domain, 'raw': domain_string})
+    else:
+        assert args.domain, \
+            'You need to specify a domain file or a domain name.'
+        domain = args.domain
+    for problempath in args.problems:
+        print('Adding problem: {}.'.format(problempath))
+        problemfile = open(problempath, 'r')
+        problem_string = problemfile.read()
+        problem = get_problemname(problem_string)
+        problem_domain = get_domain_of_problem(problem_string)
+        assert problem_domain == domain, \
+            'Domain "{}" in problem "{}" does not match ' \
+            'given domain name "{}".'.format(problem_domain, problem, domain)
+        assert domain_coll.find({ 'name': domain }).count() > 0, \
+            'Missing domain "{}" in database for problem "{}".'.format(
+                domain, problem)
+        assert problem_coll.find({ 'name': problem }).count() == 0, \
+            'Problem "{}" already exists in database.'.format(problem)
+        problem_coll.insert(
+            {'name': problem, 'domain': domain, 'raw': problem_string}
+        )
+
+if __name__ == '__main__':
+    main()
+
