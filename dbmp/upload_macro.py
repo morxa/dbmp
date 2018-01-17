6a73d67d20cba15b55839d291fabc78ef70a69ed
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2018-01-17 13:15:16 +0100

    dbmp: add script to upload a generated MacroFF macro

---
+++ dbmp/upload_macro.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+#  Created:  Tue 16 Jan 2018 15:16:12 CET
+#  Copyright  2018  Till Hofmann <hofmann@kbsg.rwth-aachen.de>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Library General Public License for more details.
+#
+#  Read the full text in the LICENSE.GPL file in the doc directory.
+
+"""
+Upload the given MacroFF macro file to the database.
+"""
+
+import argparse
+import pymongo
+
+def main():
+    arg_parser = argparse.ArgumentParser(
+        description="Upload the given MacroFF macro file to the database")
+    arg_parser.add_argument('--host', type=str, default='enterprise',
+                            help='Host to connect to')
+    arg_parser.add_argument('-u', '--user', type=str, default='planner',
+                            help='database user')
+    arg_parser.add_argument('-p', '--password', type=str,
+                            help='password of the database user')
+    arg_parser.add_argument('domain', type=str,
+                            help='the domain name')
+    arg_parser.add_argument('macro_file', type=argparse.FileType('r'))
+    args = arg_parser.parse_args()
+    client = pymongo.MongoClient(host=args.host)
+    db = client.macro_planning
+    db.authenticate(args.user, args.password)
+    print('Uploading macro file "{}" for domain "{}"'.format(
+        args.macro_file.name, args.domain))
+    db.macros.insert_one({'name': 'macroff-' + args.domain,
+                          'domain': args.domain,
+                          'planner': 'macroff',
+                          'raw': args.macro_file.read() })
+
+if __name__ == '__main__':
+    main()
